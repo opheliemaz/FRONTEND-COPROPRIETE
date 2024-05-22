02acb92fe8f7b5cf5daac9929b2260ca613bc5e6
Ophélie <opheliemazonna@outlook.fr>
2024-05-22 09:08:42 +0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Coproprietes from './components/Coproprietes';
+// autres imports
 
-function App({ Component, pageProps }) {
+function App() {
   return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/coproprietes" component={Coproprietes} />
+        {/* autres routes */}
+      </Switch>
+    </Router>
   );
 }
 
